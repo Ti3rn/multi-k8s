1a6b244521b7992315503d45eb831c5b4b1133cf
tiern <create.by.ti3n@gmail.com>
2019-09-13 17:33:40 +0700

    init commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.REDIS_PGUSER,
+  pgPassword: process.env.REDIS_PGPASSWORD,
+  pgHost: process.env.REDIS_PGHOST,
+  pgDatabase: process.env.REDIS_PGDATABASE,
+  pgPort: process.env.REDIS_PGPORT,
+};
